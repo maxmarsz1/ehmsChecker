01e42c8af6e0e87122788ac75967236c94f41db4
piayer69 <maxmarsz1@gmail.com>
2023-02-20 09:02:42 +0100

    Init

---
+++ bot.py
@@ -0,0 +1,91 @@
+API = '6046906090:AAFS8YgUMK49o4w5ooGe8-xGg77Fnu7hU8k'
+JOB_FILENAME = 'jobs'
+
+from check import checkIfNewAnn
+
+import logging
+import pickle
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+def initJobs(job_queue):
+    try:
+        chat_ids = pickle.load(open(JOB_FILENAME, 'rb'))
+        print(chat_ids)
+        for chat_id in chat_ids:
+            job_queue.run_repeating(check, 1800, chat_id=chat_id, name=str(chat_id))
+    except FileNotFoundError as e:
+        print("No jobs saved")
+
+
+def saveJobs(job_queue):
+    chat_ids = [job.chat_id for job in job_queue.jobs()]
+    pickle.dump(chat_ids, open(JOB_FILENAME, 'wb'))
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Type /subscribe to get notified when new announcements are available")
+
+
+async def check(context: ContextTypes.DEFAULT_TYPE) -> None:
+    job = context.job
+    new_ann = checkIfNewAnn()
+
+    if new_ann:
+        text = f"Nowe ogłoszenie: {new_ann}"
+        await context.bot.send_message(chat_id=job.chat_id, text=text)
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_repeating(check, 1800, chat_id=chat_id, name=str(chat_id))
+
+    if job_removed:
+        text = "Already subscribed"
+    else: 
+        text = "Subscribed"
+        saveJobs(context.job_queue)
+
+
+    await update.effective_message.reply_text(text)
+
+
+async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    saveJobs(context.job_queue)
+    text = "Unsubscribed!" if job_removed else "You have no active subscription."
+    await update.message.reply_text(text)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(API).build()
+    job_queue = application.job_queue
+
+    initJobs(job_queue)
+    
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler("subscribe", subscribe))
+    application.add_handler(CommandHandler("unsubscribe", unsubscribe))
+    
+    application.run_polling()
---
+++ check.py
@@ -0,0 +1,72 @@
+import requests
+from time import sleep, strftime
+from bs4 import BeautifulSoup 
+
+FILENAME = "announcements.txt"
+CREDS = "creds.txt"
+
+
+def lprint(text):
+    print(f"{strftime('%d.%m.%Y %H:%M:%S')}: {text}")
+
+
+def getAnnTitles():
+    url = 'https://ehms.pk.edu.pl/standard/'
+    with open(CREDS, 'r') as f:
+        login, password = [line.strip() for line in f.readlines()]
+
+    with requests.Session() as s:
+        resp = s.get(url)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        form = soup.form
+        _, login_name, password_name, counter  = form.find_all('input')
+        login_form = login_name.get('name')
+        password_form = password_name.get('name')
+        counter_form = counter.get('value')
+        data = {
+            'log_form': 'yes',
+            login_form: login,
+            password_form: password,
+            'counter': counter_form
+        }
+        s.post(url, data=data)
+        resp = s.get(url + '?select_kodf=&kodf=&tokid=431905')
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        ann = soup.tbody.find_all('tr')
+        ann_titles = "\n".join([ann.find_all('td')[1].text for ann in ann])
+
+        return ann_titles
+
+
+def saveAnn(ann):
+    with open(FILENAME, 'w+') as f:
+        f.write(ann)
+
+
+def readAnn():
+    try:
+        with open(FILENAME, 'r') as f:
+            return f.read()
+    except FileNotFoundError as e:
+        print(e)
+
+
+def checkIfNewAnn():
+    ann = getAnnTitles()
+    prev_ann = readAnn()
+
+    lprint("Sprawdzam ogłoszenia...")
+    if prev_ann != ann:
+        lprint("Nowe ogłoszenie!")
+        new_ann = ann.replace(prev_ann, '')
+    else:
+        lprint('Nic nowego')
+        new_ann= False
+
+    saveAnn(ann)
+    return new_ann
+        
+
+
+if __name__ == '__main__':
+    checkIfNewAnn()
