dd88586d3ab034d235a39ee3e1ca61ce2ab36197
piayer69 <maxmarsz1@gmail.com>
2023-02-21 23:48:01 +0100

    Persistant session implemented, changed how jobs work

--- bot.py
+++ bot.py
@@ -1,92 +1,89 @@
-with open('API_KEY', 'r') as f: 
+with open("API_KEY", "r") as f:
     API = f.read().strip()
-JOB_FILENAME = 'jobs'
+FILENAME_JOBS = "jobs.txt"
 INTERVAL = 1800
 
-from check import checkIfNewAnn, lprint
+from check import Checker
 
+import json
 import datetime
 import logging
-import pickle
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
-def initJobs(job_queue):
+def initJobs() -> list:
     try:
-        chat_ids = pickle.load(open(JOB_FILENAME, 'rb'))
-        print(chat_ids)
-        for chat_id in chat_ids:
-            job_queue.run_repeating(check, INTERVAL, chat_id=chat_id, name=str(chat_id))
-    except FileNotFoundError as e:
-        print("No jobs saved")
+        with open(FILENAME_JOBS, "r") as f:
+            data = f.read()
+    except FileNotFoundError:
+        data = '[]'
+
+    return json.loads(data)
 
 
-def saveJobs(job_queue):
-    chat_ids = [job.chat_id for job in job_queue.jobs()]
-    pickle.dump(chat_ids, open(JOB_FILENAME, 'wb'))
+def saveJobs(jobs) -> None:
+    data = json.dumps(jobs)
+    with open(FILENAME_JOBS, "w+") as f:
+        f.write(data)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Type /subscribe to get notified when new announcements are available")
-
-
-async def check(context: ContextTypes.DEFAULT_TYPE) -> None:
-    job = context.job
-    new_ann = checkIfNewAnn()
-    if new_ann:
-        text = f"Nowe ogłoszenie:\n{new_ann}"
-        await context.bot.send_message(chat_id=job.chat_id, text=text)
-
-
-def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
-    """Remove job with given name. Returns whether job was removed."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Type /subscribe to get notified when new announcements are available",
+    )
 
 
 async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Add a job to the queue."""
     chat_id = update.effective_message.chat_id
-
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_repeating(check, INTERVAL, chat_id=chat_id, name=str(chat_id))
-
-    if job_removed:
-        text = "Already subscribed"
-    else: 
+    jobs = context.bot_data["jobs"]
+    text = "Already subscribed"
+    if chat_id not in jobs:
+        jobs.append(chat_id)
         text = "Subscribed"
-        saveJobs(context.job_queue)
 
+    saveJobs(jobs)
     await update.effective_message.reply_text(text)
 
 
 async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Remove the job if the user changed their mind."""
     chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    saveJobs(context.job_queue)
-    text = "Unsubscribed!" if job_removed else "You have no active subscription."
+    jobs = context.bot_data["jobs"]
+    try:
+        jobs.remove(chat_id)
+        text = "Unsubscribed"
+    except ValueError:
+        text = "You have no active subscription."
+
+    saveJobs(jobs)
     await update.message.reply_text(text)
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(API).build()
-    job_queue = application.job_queue
+async def check(context) -> None:
+    newAnn = context.bot_data['checker'].checkIfNewAnn()
+    if newAnn:
+        for chat_id in context.bot_data['jobs']:
+            await context.bot.send_message(chat_id, text=context.bot_data['checker'].new_ann)
 
-    initJobs(job_queue)
-    
-    application.add_handler(CommandHandler('start', start))
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(API).build()
+    application.bot_data["jobs"] = initJobs()
+    application.job_queue.run_repeating(check, INTERVAL, first=10)
+
+    application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("subscribe", subscribe))
     application.add_handler(CommandHandler("unsubscribe", unsubscribe))
+
+    application.bot_data['checker'] = Checker()
+    if application.bot_data['checker'].is_authorized:
+        application.run_polling()
+
     
-    application.run_polling()
--- check.py
+++ check.py
@@ -1,76 +1,87 @@
 import requests
 from time import sleep, strftime
-from bs4 import BeautifulSoup 
-
-FILENAME = "announcements.txt"
-CREDS = "creds.txt"
+from bs4 import BeautifulSoup
 
 
-def lprint(text):
-    print(f"{strftime('%d.%m.%Y %H:%M:%S')}: {text}")
+class Checker:
+    def __init__(self):
+        self.FILENAME_ANN = "announcements.txt"
+        self.CREDS = "creds.txt"
+        self.URL = "https://ehms.pk.edu.pl/standard/"
+        self.URL_ANN = self.URL + "?select_kodf=&kodf=&tokid=431905"
+        self.session = requests.Session()
+        self.is_authorized = self.authorize()
 
+    @staticmethod
+    def lprint(text) -> None:
+        print(f"{strftime('%d.%m.%Y %H:%M:%S')}: {text}")
 
-def getAnnTitles():
-    url = 'https://ehms.pk.edu.pl/standard/'
-    with open(CREDS, 'r') as f:
-        login, password = [line.strip() for line in f.readlines()]
+    def authorize(self) -> bool:
+        with open(self.CREDS, "r") as f:
+            login, password = [line.strip() for line in f.readlines()]
 
-    with requests.Session() as s:
-        resp = s.get(url)
-        soup = BeautifulSoup(resp.text, 'html.parser')
+        resp = self.session.get(self.URL)
+        soup = BeautifulSoup(resp.text, "html.parser")
         form = soup.form
-        _, login_name, password_name, counter  = form.find_all('input')
-        login_form = login_name.get('name')
-        password_form = password_name.get('name')
-        counter_form = counter.get('value')
+        _, login_name, password_name, counter = form.find_all("input")
+        login_form = login_name.get("name")
+        password_form = password_name.get("name")
+        counter_form = counter.get("value")
         data = {
-            'log_form': 'yes',
+            "log_form": "yes",
             login_form: login,
             password_form: password,
-            'counter': counter_form
+            "counter": counter_form,
         }
-        s.post(url, data=data)
-        resp = s.get(url + '?select_kodf=&kodf=&tokid=431905')
-        soup = BeautifulSoup(resp.text, 'html.parser')
+        resp = self.session.post(self.URL, data=data)
+        return resp.status_code == 200
+
+    def getNewSoup(self) -> None:
+        resp = self.session.get(self.URL_ANN)
+        self.soup = BeautifulSoup(resp.text, "html.parser")
+
+    def getAnnTitles(self) -> str:
+        self.getNewSoup()
         try:
-            ann = soup.tbody.find_all('tr')
-            ann_titles = "\n".join([ann.find_all('td')[1].text for ann in ann])
-        except Exception as e:
+            ann = self.soup.tbody.find_all("tr")
+            ann_titles = "\n".join([ann.find_all("td")[1].text for ann in ann])
+
+        # Catching exception when tbody with announcements is no present (No announcements table present)
+        except AttributeError as e:
             print(e)
             ann_titles = ""
 
         return ann_titles
 
+    def saveAnn(self, ann) -> None:
+        with open(self.FILENAME_ANN, "w+") as f:
+            f.write(ann)
 
-def saveAnn(ann):
-    with open(FILENAME, 'w+') as f:
-        f.write(ann)
+    def readAnn(self) -> str:
+        try:
+            with open(self.FILENAME_ANN, "r") as f:
+                return f.read()
+        except FileNotFoundError as e:
+            return ""
 
+    def checkIfNewAnn(self) -> bool:
+        ann = self.getAnnTitles()
+        prev_ann = self.readAnn()
 
-def readAnn():
-    try:
-        with open(FILENAME, 'r') as f:
-            return f.read()
-    except FileNotFoundError as e:
-        print(e)
+        self.lprint("Checking announcements...")
+        if prev_ann != ann:
+            self.lprint("New announcement/s!")
+            self.new_ann = ann.replace(prev_ann, "")
+        else:
+            self.lprint("Nothing new")
+            self.new_ann = ""
 
-
-def checkIfNewAnn():
-    ann = getAnnTitles()
-    prev_ann = readAnn()
-
-    lprint("Sprawdzam ogłoszenia...")
-    if prev_ann != ann:
-        lprint("Nowe ogłoszenie!")
-        new_ann = ann.replace(prev_ann, '') if prev_ann is not None else ann
-    else:
-        lprint('Nic nowego')
-        new_ann= False
-
-    saveAnn(ann)
-    return new_ann
-        
+        self.saveAnn(ann)
+        return self.new_ann != ""
 
 
-if __name__ == '__main__':
-    checkIfNewAnn()
+if __name__ == "__main__":
+    a = Checker()
+    if a.is_authorized:
+        a.checkIfNewAnn()
+        print(a.new_ann)
