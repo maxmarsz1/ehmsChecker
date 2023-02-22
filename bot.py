386f75502c8b27a69dfe72d15d24f89ff25b169d
piayer69 <maxmarsz1@gmail.com>
2023-02-22 11:42:59 +0100

    Rerun persistant session, reduced amount of requests

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ INTERVAL = 1800
 from check import Checker
 
 import json
+import random
 import datetime
 import logging
 from telegram import Update
@@ -66,8 +67,15 @@ async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     await update.message.reply_text(text)
 
 
+def getRandomInterval():
+    if random.randint(0, 49):
+        return INTERVAL + random.randint(-900, 900)
+    return INTERVAL + random.randint(900, 1800)
+
+
 async def check(context) -> None:
     newAnn = context.bot_data['checker'].checkIfNewAnn()
+    context.job_queue.run_once(check, getRandomInterval())
     if newAnn:
         for chat_id in context.bot_data['jobs']:
             await context.bot.send_message(chat_id, text=context.bot_data['checker'].new_ann)
@@ -76,7 +84,7 @@ async def check(context) -> None:
 if __name__ == "__main__":
     application = ApplicationBuilder().token(API).build()
     application.bot_data["jobs"] = initJobs()
-    application.job_queue.run_repeating(check, INTERVAL, first=10)
+    application.job_queue.run_once(check, getRandomInterval())
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("subscribe", subscribe))
--- check.py
+++ check.py
@@ -1,6 +1,9 @@
 import requests
+import pickle
 from time import sleep, strftime
 from bs4 import BeautifulSoup
+
+FILENAME_SESSION = "session"
 
 
 class Checker:
@@ -9,49 +12,30 @@ class Checker:
         self.CREDS = "creds.txt"
         self.URL = "https://ehms.pk.edu.pl/standard/"
         self.URL_ANN = self.URL + "?select_kodf=&kodf=&tokid=431905"
-        self.session = requests.Session()
-        self.is_authorized = self.authorize()
+        self.loadSession()
+        self.session.headers.update(
+            {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.78 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+            }
+        )
+        if not self.checkAuthorized():
+            self.authorize()
+        self.checkNewAnn()
+        self.saveSession()
 
-    @staticmethod
-    def lprint(text) -> None:
-        print(f"{strftime('%d.%m.%Y %H:%M:%S')}: {text}")
+    def saveSession(self) -> None:
+        self.lprint("Saving authorized session")
+        pickle.dump(self.session, open(FILENAME_SESSION, "wb+"))
 
-    def authorize(self) -> bool:
-        with open(self.CREDS, "r") as f:
-            login, password = [line.strip() for line in f.readlines()]
-
-        resp = self.session.get(self.URL)
-        soup = BeautifulSoup(resp.text, "html.parser")
-        form = soup.form
-        _, login_name, password_name, counter = form.find_all("input")
-        login_form = login_name.get("name")
-        password_form = password_name.get("name")
-        counter_form = counter.get("value")
-        data = {
-            "log_form": "yes",
-            login_form: login,
-            password_form: password,
-            "counter": counter_form,
-        }
-        resp = self.session.post(self.URL, data=data)
-        return resp.status_code == 200
-
-    def getNewSoup(self) -> None:
-        resp = self.session.get(self.URL_ANN)
-        self.soup = BeautifulSoup(resp.text, "html.parser")
-
-    def getAnnTitles(self) -> str:
-        self.getNewSoup()
+    def loadSession(self) -> None:
+        self.lprint("Loading saved session")
         try:
-            ann = self.soup.tbody.find_all("tr")
-            ann_titles = "\n".join([ann.find_all("td")[1].text for ann in ann])
-
-        # Catching exception when tbody with announcements is no present (No announcements table present)
-        except AttributeError as e:
-            print(e)
-            ann_titles = ""
-
-        return ann_titles
+            self.session = pickle.load(open(FILENAME_SESSION, "rb"))
+        except FileNotFoundError:
+            self.session = requests.Session()
 
     def saveAnn(self, ann) -> None:
         with open(self.FILENAME_ANN, "w+") as f:
@@ -64,7 +48,58 @@ class Checker:
         except FileNotFoundError as e:
             return ""
 
-    def checkIfNewAnn(self) -> bool:
+    @staticmethod
+    def lprint(text) -> None:
+        print(f"{strftime('%d.%m.%Y %H:%M:%S')}: {text}")
+
+    def authorize(self) -> None:
+        self.lprint("Authorizing")
+        with open(self.CREDS, "r") as f:
+            login, password = [line.strip() for line in f.readlines()]
+
+        _, login_name, password_name, counter = self.form.find_all("input")
+        login_form = login_name.get("name")
+        password_form = password_name.get("name")
+        counter_form = counter.get("value")
+        data = {
+            "log_form": "yes",
+            login_form: login,
+            password_form: password,
+            "counter": counter_form,
+        }
+        resp = self.session.post(self.URL, data=data)
+        if "niepoprawny UID" in resp.text:
+            raise Exception("Failed to authorize")
+        self.soup = BeautifulSoup(resp.text, "html.parser")
+
+    def checkAuthorized(self) -> bool:
+        self.lprint("Checking access")
+        resp = self.session.get(self.URL_ANN)
+        is_authorized = "Logowanie do systemu" not in resp.text
+        if is_authorized:
+            self.soup = BeautifulSoup(resp.text, "html.parser")
+        else:
+            self.form = BeautifulSoup(resp.text, "html.parser").form
+        return is_authorized
+
+    def getNewSoup(self) -> None:
+        self.lprint("Getting new soup")
+        if not self.checkAuthorized():
+            self.authorize()
+
+    def getAnnTitles(self) -> str:
+        try:
+            ann = self.soup.tbody.find_all("tr")
+            ann_titles = "\n".join([ann.find_all("td")[1].text for ann in ann])
+
+        # Catching exception when tbody with announcements is no present (No announcements table present)
+        except AttributeError as e:
+            print(e)
+            ann_titles = ""
+
+        return ann_titles
+
+    def checkNewAnn(self) -> bool:
         ann = self.getAnnTitles()
         prev_ann = self.readAnn()
 
@@ -82,6 +117,4 @@ class Checker:
 
 if __name__ == "__main__":
     a = Checker()
-    if a.is_authorized:
-        a.checkIfNewAnn()
-        print(a.new_ann)
+    print(a.new_ann)
